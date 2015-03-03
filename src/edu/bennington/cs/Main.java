84ff9c48a0a50cb3fc2ac6add9939cfdd85e5825
Chitrakar <nideshchitrakar@bennington.edu>
2015-03-03 12:59:38 -0500

    Initial commit

---
+++ src/edu/bennington/cs/Main.java
@@ -0,0 +1,11 @@
+package edu.bennington.cs;
+
+public class Main {
+    public static int i;
+    public static char c;
+
+    public static void main(String[] args) {
+        System.out.println("int = " + i);
+        System.out.println("char = " + c);
+    }
+}
